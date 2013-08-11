5a8b7e08f68ec7f61a67b6a231249a2d3e6965e7
Rocky He <rocky.jj.he@gmail.com>
2013-08-11 00:13:59 -0700

    Added updating of item stock after returning an item

--- createReturn.php
+++ createReturn.php
@@ -11,8 +11,7 @@ $retid=0;
 //create an entry in Returns table
 $date_now_str = date("Y-m-d H:i:s");
 // $date_now_datetime = new DateTime($date_now_str);
-$return_sql = "INSERT INTO returns (ret_date, receiptId)
-			VALUES ('$date_now_str', '$receiptId')";
+$return_sql = "INSERT INTO returns (ret_date, receiptId) VALUES ('$date_now_str', '$receiptId')";
 
 if (!mysql_query($return_sql))
 {
@@ -25,8 +24,7 @@ else
 }
 
 //create an entry in ReturnItem table for the given item upc
-$ret_item_sql = "INSERT INTO return_item (retid, upc, quantity)
-						VALUES ('$retid', '$upc', '$qty')";
+$ret_item_sql = "INSERT INTO return_item (retid, upc, quantity) VALUES ('$retid', '$upc', '$qty')";
 
 if (!mysql_query($ret_item_sql))
 {
@@ -35,6 +33,17 @@ if (!mysql_query($ret_item_sql))
 else
 {
 	echo "1 ReturnItem added";
+}
+
+//update the stock for the corresponding item
+$item_sql = "UPDATE item SET stock = stock+'$qty' WHERE upc = '$upc'";
+if (!mysql_query($item_sql))
+{
+	echo "Error: " . mysql_error();
+}
+else
+{
+	echo "Updated stock for item upc: " . $upc; 
 }
 
 mysql_close();
