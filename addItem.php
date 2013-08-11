dddc142b22f5b33859904d864f746b61a3b41015
Bo Yang Hu <boyangh@gmail.com>
2013-08-10 17:33:20 -0700

    added manager

---
+++ addItem.php
@@ -0,0 +1,22 @@
+<?php
+
+// connect with the database
+include_once("MYSQL_Connect.php");
+
+//if(isset($_POST["upc"])) { $upc=$_POST["upc"]; }
+$title=$_GET["title"]; 
+$disk_type=$_GET["disk_type"]; 
+$category=$_GET["category"]; 
+$company=$_GET["company"]; 
+$pub_year=$_GET["pub_year"]; 
+$price=$_GET["price"];
+$stock=$_GET["stock"]; 
+
+$query=mysql_query("INSERT INTO item(title,disk_type,category,company,pub_year,price,stock) 
+					VALUES('$title','$disk_type','$category','$company','$pub_year','$price','$stock')");
+if($query){
+echo "Data for $title inserted successfully!";
+}
+else{ echo "An error occurred!"; }
+
+?>
---
+++ addstock.php
@@ -0,0 +1,24 @@
+<?php
+
+// connect with the database
+include_once("MYSQL_Connect.php");
+
+$upc=$_GET["upc"];
+$stock=$_GET["stock"];
+$price=$_GET["price"];
+
+if(empty($_GET["price"])) {
+	$query=mysql_query("UPDATE item
+					SET stock=stock+'$stock'
+					WHERE upc='$upc'");
+} else {
+	$query=mysql_query("UPDATE item
+						SET stock=stock+'$stock',price='$price'
+						WHERE upc='$upc'");
+}
+if($query){
+echo "Stock for $upc inserted successfully!";
+}
+else{ echo "An error occurred!"; }
+
+?>
